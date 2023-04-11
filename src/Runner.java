0bbb31b4a644d51a45c1690d337c7e2156f41633
TheDark <104472362+99TheDark@users.noreply.github.com>
2023-04-11 01:48:59 -0700

    Update Runner.java

--- src/Runner.java
+++ src/Runner.java
@@ -1,7 +1,8 @@
-import org.thedark.Array;
+import org.thedark.utils.Array;
 
 public class Runner {
 
+	@SuppressWarnings("unused")
 	public static void main(String[] args) {
 
 		Array<String> names = new Array<String>();
@@ -11,21 +12,29 @@ public class Runner {
 		names.add("Gary");
 		names.add("Jim", "John", "Dave", "Eric", "Benjamin", "Terry", "Gabe", "Tim", "Tom", "Isaac", "Noah");
 		names.insert("Jennifer", 5);
+		names.insert("Josh", 0);
+		names.add("Quinn");
 		
 		names.sort((a, b) -> a.compareToIgnoreCase(b));
+		
+		Array<Array<String>> tables = names.chunk(5);
+		
+		Array<String> sentance = new Array<String>();
 
-		Array<String> letters = new Array<String>();
-
-		letters.add("H", "e", "l", "l", "o");
-		letters.add(" ");
-		letters.add("t", "h", "e", "r", "e", "!");
+		sentance.add("H", "e", "l", "l", "o");
+		sentance.add(" ");
+		sentance.add("t", "h", "e", "r", "e", "!");
 		
 		Array<Integer> nums = new Array<Integer>();
 		nums.add(38, 27, 43, 3, 9, 82, 10);
 		nums.sort((Integer a, Integer b) -> a - b);
 		
-		System.out.println(nums.reduce((val, cur) -> cur + val, 0));
-				
+		int sum = nums.reduce((val, cur) -> cur + val, 0);
+		
+		Array<Character> letters = new Array<Character>();
+		letters.add('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H');
+		letters.shuffle();
+																
 	}
 
 }
