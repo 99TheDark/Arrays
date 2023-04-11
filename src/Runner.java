1e4b527a2566bd7389dd746d68f6fbf78d5f7ccd
TheDark <104472362+99TheDark@users.noreply.github.com>
2023-04-11 02:18:59 -0700

    Update Runner.java

--- src/Runner.java
+++ src/Runner.java
@@ -14,27 +14,27 @@ public class Runner {
 		names.insert("Jennifer", 5);
 		names.insert("Josh", 0);
 		names.add("Quinn");
-		
+
 		names.sort((a, b) -> a.compareToIgnoreCase(b));
-		
+
 		Array<Array<String>> tables = names.chunk(5);
-		
+
 		Array<String> sentance = new Array<String>();
 
 		sentance.add("H", "e", "l", "l", "o");
 		sentance.add(" ");
 		sentance.add("t", "h", "e", "r", "e", "!");
-		
+
 		Array<Integer> nums = new Array<Integer>();
 		nums.add(38, 27, 43, 3, 9, 82, 10);
 		nums.sort((Integer a, Integer b) -> a - b);
-		
+
 		int sum = nums.reduce((val, cur) -> cur + val, 0);
-		
+
 		Array<Character> letters = new Array<Character>();
 		letters.add('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H');
 		letters.shuffle();
-																
+
 	}
 
 }
